92d935a89e9ee3ac08ad731eb5f0cb70f51c31a7
hyunjin Lee <web.creastory@gmail.com>
2024-01-13 21:48:48 +0900

    starhub commit1

--- src/App.js
+++ src/App.js
@@ -25,11 +25,11 @@ function App() {
         <Route path='signup' element={<SignUp />} />
         <Route path='mypage' element={<Mypage />} />
         <Route path='myprofile' element={<MyProfile />} />
-        <Route path='Password' element={<Password />} />
+        <Route path='password' element={<Password />} />
         // starhubpages 구성
         <Route path='starhub' element={<StarHub />} />
-        <Route path='Starboard' element={<StarBoard />} />
-        <Route path='StHubdetail' element={<StHubDetail />} />
+        <Route path='starboards' element={<StarBoard />} />
+        <Route path='sthubdetail' element={<StHubDetail />} />
         // starsharepages 구성
         <Route path='starshare' element={<StarShare />} />
         <Route path='stshrdetail' element={<StShrDetail />} />
--- src/pages/homepages/Login.jsx
+++ src/pages/homepages/Login.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
-  const Navigate = useNavigate()
+    const Navigate = useNavigate();
 
-  return (
-    <div>Login
-      <br />
-      <button onClick={()=>{
-        Navigate('/signup')
-      }}>가입하기</button>
-    </div>
-  )
+    return (
+        <div>
+            Login
+            <br />
+            <button
+                onClick={() => {
+                    Navigate('/starboards');
+                }}
+            >
+                스타보드
+            </button>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
 // 로그인 페이지
