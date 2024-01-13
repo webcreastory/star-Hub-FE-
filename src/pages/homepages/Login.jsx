48adf568bf5f300864c53210920d77e302841df2
jaehyun <ijh9147@gmail.com>
2024-01-13 21:26:07 +0900

    로그인 회원가입 기능 구현

--- src/pages/homepages/Login.jsx
+++ src/pages/homepages/Login.jsx
@@ -1,18 +1,95 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import api from '../../api/api';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import Navbar from './Navbar';
 
-function Login() {
-  const Navigate = useNavigate()
+function useInput(initialValue = '') {
+  const [value, setValue] = useState(initialValue);
 
-  return (
-    <div>Login
-      <br />
-      <button onClick={()=>{
-        Navigate('/signup')
-      }}>가입하기</button>
-    </div>
-  )
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return [value, handleChange];
 }
 
-export default Login
-// 로그인 페이지
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useInput('');
+  const [password, setPassword] = useInput('');
+
+  const navigateToSignup = () => {
+    navigate('/signup');
+  };
+
+  const loginButton = async () => {
+    try {
+      if (email === '' || password === '') {
+        alert('아이디와 비밀번호를 입력해주세요');
+        return;
+      }
+
+      const response = await api.post(`/login`, { email, password });
+      Cookies.set('token', response.data.token);
+      navigate('/starhub');
+    } catch (error) {
+      if (error.response) {
+        const statusCode = error.response.status;
+        const errorMessage = error.response.data.message;
+
+        if (statusCode === 400) {
+          alert(errorMessage);
+        }
+      }
+    }
+  };
+
+  return (
+    <div style={{
+      margin: '10px',
+      marginTop: '100px'
+    }}>
+      <Navbar />
+      <div>
+        <h1>로그인</h1>
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            loginButton();
+          }}
+        >
+          <div>이메일</div>
+          <input
+            type="text"
+            value={email}
+            onChange={setEmail}
+            placeholder='이메일을 입력해주세요'
+          />
+          <div>비밀번호</div>
+          <input
+            type="password"
+            value={password}
+            onChange={setPassword}
+            placeholder='비밀번호를 입력해주세요'
+          />
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignContent: 'center',
+            }}
+          >
+            <button>로그인</button>
+            <button onClick={navigateToSignup}>회원가입</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/homepages/SignUp.jsx
+++ src/pages/homepages/SignUp.jsx
@@ -1,18 +1,138 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import api from '../../api/api';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar';
 
-function SignUp() {
-  const Navigate = useNavigate()
+function useInput(initialValue = '') {
+  const [value, setValue] = useState(initialValue);
 
-  return (
-    <div>SignUp
-      <br />
-      <button onClick={()=>{
-        Navigate('/')
-      }}>로그인</button>
-    </div>
-  )
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return [value, handleChange];
 }
 
-export default SignUp
-// 회원가입하기
+function SignUp() {
+  const [email, setEmail] = useInput('');
+  const [password, setPassword] = useInput('');
+  const [name, setName] = useInput('');
+  const [major, setMajor] = useInput('');
+  const navigate = useNavigate();
+  const [emailValid, setEmailValid] = useState(null);
+  const [passwordValid, setPasswordValid] = useState(null);
+
+  useEffect(() => {
+    if (email !== '') {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      setEmailValid(emailRegex.test(email));
+    } else {
+      setEmailValid(null);
+    }
+
+    if (password !== '') {
+      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,15}$/;
+      setPasswordValid(passwordRegex.test(password));
+    } else {
+      setPasswordValid(null);
+    }
+  }, [email, password]);
+
+  const navigateToLogin = () => {
+    navigate('/');
+  };
+
+  const signUpButton = async () => {
+    if (email === '' || password === '' || name === '' || major === '') {
+      alert("빈칸을 모두 작성해주세요");
+      return;
+    }
+
+    if (!emailValid || !passwordValid) {
+      alert("입력한 정보를 다시 확인해주세요");
+      return;
+    }
+
+    try {
+      const response = await api.post(`/signup`, { name, password, email, major });
+      if (response.status === 201) {
+        alert("회원가입에 성공했습니다.");
+        navigateToLogin();
+      }
+    } catch (error) {
+      if (error.response) {
+        const statusCode = error.response.status;
+        const errorMessage = error.response.data.message;
+        if (statusCode === 400) {
+          alert(errorMessage);
+        }
+      }
+    }
+  };
+
+  return (
+    <div style={{ margin: '10px', marginTop: '100px' }}>
+      <Navbar />
+      <div>
+        <h1>회원가입</h1>
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            signUpButton();
+          }}
+        >
+          <div>반을 선택해주세요</div>
+          <select value={major} onChange={setMajor}>
+            <option value="" disabled>반을 선택해주세요</option>
+            <option value="R반">R반</option>
+            <option value="S반">S반</option>
+            <option value="N반">N반</option>
+          </select>
+          <div>이름</div>
+          <input
+            type='text'
+            value={name}
+            onChange={setName}
+            placeholder='이름을 입력해주세요'
+          />
+          <div>E-mail</div>
+          <input
+            type='text'
+            value={email}
+            onChange={setEmail}
+            placeholder='아이디로 사용할 E-mail을 입력해주세요'
+          />
+          {emailValid === null ? null : (
+            emailValid ? (
+              <span style={{ color: 'green' }}>사용 가능합니다</span>
+            ) : (
+              <span style={{ color: 'red' }}>다시 입력해주세요</span>
+            )
+          )}
+          <div>Password</div>
+          <input
+            type='password'
+            value={password}
+            onChange={setPassword}
+            placeholder='비밀번호는 대문자/소문자/특수문자를 포함하여 8~15자 이내로 작성해주세요'
+          />
+          {passwordValid === null ? null : (
+            passwordValid ? (
+              <span style={{ color: 'green' }}>사용 가능합니다</span>
+            ) : (
+              <span style={{ color: 'red' }}>비밀번호는 대문자/소문자/특수문자를 포함하여 8~15자 이내로 작성해주세요</span>
+            )
+          )}
+
+          <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center' }}>
+            <button>회원가입</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
